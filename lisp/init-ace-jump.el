428a464813692d40c5dc86a60f5b258ea234602c
Peyo Tzolov <ptzolov@accedian.com>
2018-10-09 15:12:09 -0400

    ace-jump mode shortcut

--- lisp/init-ace-jump.el
+++ lisp/init-ace-jump.el
@@ -1,7 +1,5 @@
 ;; Use smex to handle M-x
-(require-package 'ace-jump-mode)
-
-(after-load 'ace-jump-mode
+(when (require-package 'ace-jump-mode)
   (global-set-key (kbd "C-o") 'ace-jump-char-mode))
 
 (provide 'init-ace-jump)
